8ad0f1dc047aaa836a57cfc1e9aa0af8ee705e16
Florian Eder <florian.eder@idm-energie.at>
2024-10-14 10:36:30 +0200

    Add: new file

---
+++ python_ex1/ederfl.py
@@ -0,0 +1,14 @@
+list1 = [1, 2, 3, 4, 5]
+list2 = [11, 12, 13, 14]
+
+list_zip = []
+i = 0
+
+# Schleife läuft bis zur Länge der kleineren Liste (in diesem Fall list2)
+while i < len(list2):
+    list_zip.append(list1[i])
+    list_zip.append(list2[i])
+    i += 1
+
+print(list_zip)
+
