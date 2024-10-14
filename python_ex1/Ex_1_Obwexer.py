ad68f2ae4a4f56c2e4d55c62816817706e2df0d4
Tobias Obwexer <ot8569@mci4me.at>
2024-10-14 11:42:42 +0200

    feat: added the code for List zip reverse and list odd

--- python_ex1/Ex_1_Obwexer.py
+++ python_ex1/Ex_1_Obwexer.py
@@ -1,4 +1,3 @@
-
 list1 =[1, 2, 3, 4, 5]
 list2=[11, 12, 13, 14, 15]
 list_zip = []
@@ -8,5 +7,18 @@ for i in list1:
   list_zip.append(list2[i - 1])
 print(list_zip)
 
+list_zip_reverse = []
+for i in list1:
+  list_zip_reverse.append(list1[0 - i])
+  list_zip_reverse.append(list2[0 - i])
+print(list_zip_reverse)
 
+list_odd = []
 
+for i in list1:
+  if i % 2 != 0:
+    list_odd.append(i)
+for i in list2:
+  if i % 2 != 0:
+    list_odd.append(i)
+print(list_odd)
