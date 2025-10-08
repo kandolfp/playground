0cdacbf74b5cb3c5a1477cf4b75a1c5926ca18ae
H-Manuel <manuhofer2003@gmail.com>
2025-10-08 15:24:20 +0200

    feat: added file hm2799.py

---
+++ python_ex1/hm2799.py
@@ -0,0 +1,26 @@
+import copy
+list1 = [[1, 2, 3], [4, 5, 6], ['a', 'b', 'c']]
+list2 = list1
+list3 = copy.copy(list1)
+list4 = copy.deepcopy(list1)
+print('List # \tID\tEntries')
+print('1\t', id(list1), '\t', list1)
+print('2\t', id(list2), '\t', list2)
+print('3\t', id(list3), '\t', list3)
+print('4\t', id(list4), '\t', list4)
+list2[2][2] = 9
+print('List # \tID\tEntries')
+print('1\t', id(list1), '\t', list1)
+print('2\t', id(list2), '\t', list2)
+print('3\t', id(list3), '\t', list3)
+print('4\t', id(list4), '\t', list4)
+list1.append([0, 8, 15])
+print('1\t', id(list1), '\t', list1)
+print('2\t', id(list2), '\t', list2)
+print('3\t', id(list3), '\t', list3)
+print('4\t', id(list4), '\t', list4)
+
+
+# Be careful when copying and referencing data structures!
+# = is referencing to the original variable
+# copy.copy() is a real copy (more memory)
